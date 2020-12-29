bfa7192a847d9a59f64e78c3b6704f261d09d80d
mohanliu <mohan@u.northwestern.edu>
2020-12-28 22:47:09 -0600

    update backup scripts

--- backup.sh
+++ backup.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
 
+cd ./html
+rm -rf *
+cd ..
+
 for file in `find /var/www/html/ -user root`
     do
-    echo "Copying " $file "..."
-    cp $file ./html
+    if [ ! -d $file ]
+       then
+       echo "Copying " $file "..."
+       rsync -R $file ./html
+    fi
 done
+
+cd ./html
+mv var/www/html/* .
+rm -rf var
+cd ..
 
 echo "Copying bash scripts..."
 cp ~/bin/* bin 
